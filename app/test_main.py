7f6befc8b27edacfc551b30fe887d434e51ea5db
kene Igweagu <kodigweagu@gmail.com>
2022-01-27 01:50:47 -0500

    four running tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+import requests_mock
 from fastapi.testclient import TestClient
 from main import app
 
@@ -10,3 +11,15 @@ def test_get_users():
 def test_get_most_frequent():
     response = client.get("/most-frequent")
     assert response.status_code == 200
+
+def test_error_users():
+    with requests_mock.Mocker() as mock:
+        mock.get('/users', status_code=500)
+        response = client.get("/users")
+        assert response.status_code == 500
+
+def test_error_most_frequent():
+    with requests_mock.Mocker() as mock:
+        mock.get('/most-frequent', status_code=500)
+        response = client.get("/most-frequent")
+        assert response.status_code == 500
